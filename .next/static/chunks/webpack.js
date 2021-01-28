fae56981a11047dbc4a4c188f8b0a2673c688232
rich-cherry <rich.m.cherry@gmail.com>
2021-01-27 23:41:52 -0500

    created initial landing page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c09994d981ae200e753e";
+/******/ 	var hotCurrentHash = "928cb54587e738e3f1ed";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
